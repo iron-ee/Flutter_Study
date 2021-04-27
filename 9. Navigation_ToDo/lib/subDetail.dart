c5fe7a2e55087c11e2467d46bf9ec72e91392779
irone <doyeck@naver.com>
2021-04-27 21:56:38 +0900

    9. 내비게이션으로 페이지 이동 응용하기(To Do)

---
+++ 9. Navigation_ToDo/lib/subDetail.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class SubDetail extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SubDetail();
+}
+
+class _SubDetail extends State<SubDetail> {
+  List<String> todoList = List();
+
+  @override
+  void initState() {
+    super.initState();
+    todoList.add('공부하기');
+    todoList.add('청소하기');
+    todoList.add('드라마 보기');
+    todoList.add('주문 시키기');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sub Detail Example'),
+      ),
+      body: ListView.builder(itemBuilder: (context, index) {
+        return Card(
+          child: InkWell(
+            child: Text(
+              todoList[index],
+              style: TextStyle(fontSize: 30.0),
+            ),
+            onTap: () {
+              Navigator.of(context)
+                  .pushNamed('/third', arguments: todoList[index]);
+            },
+          ),
+        );
+      }, itemCount: todoList.length,),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addNavigation(context);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _addNavigation(BuildContext context) async {
+    final result = await Navigator.of(context).pushNamed('/second');
+    setState(() {
+      todoList.add(result);
+    });
+  }
+}
---
+++ 9. Navigation_ToDo/lib/thirdDetail.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ThirdDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // 라우터로 전달받은 데이터 가져오기
+    final String args = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Third Page'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text(
+            args,
+            style: TextStyle(fontSize: 30.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
