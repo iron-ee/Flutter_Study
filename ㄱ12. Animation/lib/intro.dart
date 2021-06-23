bacfa5a54ae7d2a637ce9b6d8a036846085c4540
irone <doyeck@naver.com>
2021-06-23 10:12:14 +0900

    12. Animation 활용해보기

---
+++ ㄱ12. Animation/lib/intro.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:animation_example/saturnLoading.dart';
+import 'dart:async';
+import 'main.dart';
+
+class IntroPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _IntroPage();
+}
+
+class _IntroPage extends State<IntroPage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  // 5초 후 메인 화면으로 이동하는 함수 정의
+  Future<Timer> loadData() async {
+    return Timer(Duration(seconds: 5), onDoneLoading);
+  }
+
+  onDoneLoading() async {
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (context) => AnimationApp()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text('애니메이션 앱'),
+              SizedBox(
+                height: 20,
+              ),
+              SaturnLoading() // 애니메이션 불러오기
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
